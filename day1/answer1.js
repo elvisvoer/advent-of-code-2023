9370ae4753116e86e4c1388bd6731068f80fa16e
Elvis Adomnica <elvis.adomnica@gmail.com>
2023-12-03 08:06:38 +0100

    Replace let with const

--- day1/answer1.js
+++ day1/answer1.js
@@ -1,6 +1,6 @@
 const result = input.split("\n").reduce((acc, cur) => {
-    let numbers = cur.split("").reduce((a, c) => {
-        let digit = parseInt(c, 10);
+    const numbers = cur.split("").reduce((a, c) => {
+        const digit = parseInt(c, 10);
         if (!isNaN(digit)) {
             a.push(digit);
         }
@@ -10,7 +10,6 @@ const result = input.split("\n").reduce((acc, cur) => {
     if (numbers.length) {
         acc += numbers[0] * 10 + numbers[numbers.length - 1];
     }
-
 
     return acc;
 }, 0);
