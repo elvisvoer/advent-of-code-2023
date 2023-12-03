ffb66c7c9c571f43f1018c6f7e32d60bb3e9b372
Elvis Adomnica <elvis.adomnica@gmail.com>
2023-12-03 08:07:19 +0100

    Replace let with const and remove console log

--- day1/answer2.js
+++ day1/answer2.js
@@ -1,5 +1,5 @@
 const result = input.split("\n").reduce((acc, cur) => {
-  let numbers = cur.match(/one|two|three|four|five|six|seven|eight|nine|\d/g).map(el => {
+  const numbers = cur.match(/one|two|three|four|five|six|seven|eight|nine|\d/g).map(el => {
     return ({
       "one": 1,
       "two": 2,
@@ -13,12 +13,9 @@ const result = input.split("\n").reduce((acc, cur) => {
     })[el] || parseInt(el, 10);
   });
 
-  console.log(numbers);
-
   if (numbers.length) {
     acc += numbers[0] * 10 + numbers[numbers.length - 1];
   }
-
 
   return acc;
 }, 0);
