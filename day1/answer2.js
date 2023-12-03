90490f1c3ef310ee48de2f4aa91f6b969cba7392
Elvis Adomnica <elvis.adomnica@gmail.com>
2023-12-03 08:05:21 +0100

    Fix identation

--- day1/answer2.js
+++ day1/answer2.js
@@ -1,24 +1,24 @@
 const result = input.split("\n").reduce((acc, cur) => {
-	let numbers = cur.match(/one|two|three|four|five|six|seven|eight|nine|\d/g).map(el => {
-	return ({
-		"one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9
-  })[el] || parseInt(el, 10);
-});
+  let numbers = cur.match(/one|two|three|four|five|six|seven|eight|nine|\d/g).map(el => {
+    return ({
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+      "five": 5,
+      "six": 6,
+      "seven": 7,
+      "eight": 8,
+      "nine": 9
+    })[el] || parseInt(el, 10);
+  });
 
   console.log(numbers);
 
-  if(numbers.length) {
-		acc += numbers[0] * 10 + numbers[numbers.length -1];
+  if (numbers.length) {
+    acc += numbers[0] * 10 + numbers[numbers.length - 1];
   }
-  
+
 
   return acc;
 }, 0);
