348a4081bfdf30682639de9fe18a718cb43ca398
Elvis Adomnica <elvis.adomnica@gmail.com>
2023-12-03 07:37:54 +0100

    Add answer to first problem

---
+++ day1/answer1.js
@@ -0,0 +1,18 @@
+const result = input.split("\n").reduce((acc, cur) => {
+    let numbers = cur.split("").reduce((a, c) => {
+        let digit = parseInt(c, 10);
+        if(!isNaN(digit)) {
+            a.push(digit);
+    }    
+        return a;
+  }, []);
+
+  if(numbers.length) {
+        acc += numbers[0] * 10 + numbers[numbers.length -1];
+  }
+  
+
+  return acc;
+}, 0);
+
+console.log(result);
